0d8fa553ff588656076bf5f317a26e682e3258ba
Aly Saleh <aly.saleh@tum.de>
2016-12-13 14:28:25 +0100

    add(scampi-api): a hello world project for Scampi API that could be exported as an execuatble jar file

---
+++ code/scampi/README.md
@@ -0,0 +1,13 @@
+# This is an Example project for SCAMPI API that can be exported as an executable jar file.
+-------------------------------------------------------------------------------------------
+
+
+
+### To create an executable jar
+--------------------------------
+```mvn clean compile assembly:single```
+
+
+## To compile
+--------------------------------
+```mvn clean install```
---
+++ code/scampi/src/main/java/com/scampi/api/ScampiHelloWorld.java
@@ -0,0 +1,87 @@
+package com.scampi.api;
+
+import fi.tkk.netlab.dtn.scampi.applib.*;
+
+/**
+ * Sample Scampi application that sends and receives Hello World! messages.
+ * For explanation see the Scampi Application Developer Guide.
+ *
+ * @author teemuk
+ */
+
+
+public class ScampiHelloWorld {
+    static private final AppLib APP_LIB = AppLib.builder().build();
+
+    public static void main( String[] args )
+            throws InterruptedException {
+        // Setup
+        APP_LIB.start();
+        APP_LIB.addLifecycleListener( new LifeCyclePrinter() );
+        APP_LIB.connect();
+
+        // Subscribe to a service
+        APP_LIB.addMessageReceivedCallback( new MessagePrinter() );
+        APP_LIB.subscribe( "Hello Service" );
+
+        // Publish a message
+        APP_LIB.publish( getMessage( "Hello World!" ), "Hello Service" );
+
+        APP_LIB.addHostDiscoveryCallback(new HostDiscoveryCallback() {
+            public void hostDiscovered(AppLib appLib, String s, int i, long l, double v, double v1, double v2) {
+                System.out.println(s);
+            }
+        });
+    }
+
+    private static SCAMPIMessage getMessage(String text ) {
+        SCAMPIMessage message = SCAMPIMessage.builder()
+                .appTag( "Hello" )
+                .build();
+        message.putString( "text", text );
+        return message;
+    }
+
+    private static final class LifeCyclePrinter
+            implements AppLibLifecycleListener {
+
+
+        public void onConnected( String scampiId ) {
+            System.out.println( "> onConnected: " + scampiId );
+        }
+
+
+        public void onDisconnected() {
+            System.out.println( "> onDisconnected" );
+        }
+
+
+        public void onConnectFailed() {
+            System.out.println( "> onConnectFailed" );
+        }
+
+
+        public void onStopped() {
+            System.out.println( "> onStopped" );
+        }
+    }
+
+    private static final class MessagePrinter
+            implements MessageReceivedCallback {
+
+
+        public void messageReceived( SCAMPIMessage message,
+                                     String service ) {
+            try {
+                if ( message.hasString( "text" ) ) {
+                    System.out.println( "> messageReceived: "
+                            + message.getString( "text" )
+
+                    );
+                }
+            } finally {
+                message.close();
+            }
+        }
+    }
+}
