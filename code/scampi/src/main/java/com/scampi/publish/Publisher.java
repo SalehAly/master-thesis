8666ff9b06545dfd23ca1bb6a72937b1fb840189
Aly Saleh <aly.saleh@tum.de>
2017-02-08 17:01:14 +0100

    fix(code): fixed the publisher to exit after publishing

--- code/scampi/src/main/java/com/scampi/publish/Publisher.java
+++ code/scampi/src/main/java/com/scampi/publish/Publisher.java
@@ -12,33 +12,30 @@ import java.util.Arrays;
  * Created by Aly on 1/23/17.
  */
 public class Publisher {
-    public static void main(String[] args){
-       String flow = null;
-        try{
-             flow = args[0];
-        }catch (Exception e ){
+    public static void main(String[] args) {
+        String flow = null;
+        try {
+            flow = args[0];
+        } catch (Exception e) {
             System.out.println("Please include a flow");
-           // e.printStackTrace();
+            // e.printStackTrace();
             return;
         }
         System.out.println(flow);
-        ScampiHelloWorld.init();
-
-        SCAMPIMessage message = SCAMPIMessage.builder().build();
-        JsonObject json = new JsonObject();
-        JsonArray flowArray = new JsonParser().parse(flow).getAsJsonArray();
-
-        json.add("flow",flowArray);
-        message.putString( "json", json.toString() );
 
         try {
+            ScampiHelloWorld.init();
+            SCAMPIMessage message = SCAMPIMessage.builder().build();
+            JsonObject json = new JsonObject();
+            JsonArray flowArray = new JsonParser().parse(flow).getAsJsonArray();
+            json.add("flow", flowArray);
+            message.putString("json", json.toString());
             ScampiHelloWorld.publish(message);
-        } catch (InterruptedException e) {
+
+        } catch (Exception e) {
             System.out.println("Error publishing the message");
             e.printStackTrace();
         }
-
         System.exit(0);
-
     }
 }
